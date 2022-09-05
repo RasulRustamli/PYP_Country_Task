4bd03a54043bb99308beec6e831f6398c9f20679
RasulRustamli <rasular@code.edu.az>
2022-09-06 01:38:00 +0400

    Add project files.

---
+++ PYP_Country_Task/Model/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PYP_Country_Task.Model;
+
+public class Country
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string ISO { get; set; }
+    public string Capital { get; set; }
+    public string Currency { get; set; }
+    public string Continent { get; set; }
+}
